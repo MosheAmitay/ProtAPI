85de3c6cf9afe07c89288fb1b4d4dd64cfa75fcc
Ellie7922 <47000746+Ellie7922@users.noreply.github.com>
2019-03-25 16:32:09 +0200

    Update UniprotApi.py

--- UniprotApi.py
+++ UniprotApi.py
@@ -3,9 +3,11 @@ import xmltodict
 import urllib2
 import json
 
+# all the queries here are meant to help extract specific details from a long and messy xml 
 
-#uniprot
+#uniprot base url
 base_uni='https://www.uniprot.org/uniprot/P12345'
+
 
 
 #query to print gene name
@@ -13,8 +15,9 @@ query_uniGeneName="&limit=1&format=xml"
 url_uniGeneName =  base_uni + query_uniGeneName
 u = requests.get(url_uniGeneName)
 docGeneName = xmltodict.parse(u.content)
+#print json.dumps(docGeneName, indent = 4)  this here would give the whole xml in a neat format 
 print "\nGene Name: "
-print json.dumps(docGeneName['uniprot']['entry']['gene']['name']['#text'], indent = 4),"\n"
+print json.dumps(docGeneName['uniprot']['entry']['gene']['name']['#text'], indent = 4),"\n" #here we go onto the xml and extract the specific data we want
 
 #query to print organism name
 query_uniOrg="&limit=1&format=xml"
@@ -22,8 +25,9 @@ url_uniOrg =  base_uni + query_uniOrg
 u = requests.get(url_uniOrg)
 docOrgName = xmltodict.parse(u.content)
 print "\nOrganism Name: "
-print json.dumps( docOrgName['uniprot']['entry']['organism']['name'][0]['#text'], indent=4),"\n"
-#print json.dumps( docOrgName['uniprot']['entry']['organism']['name'][1]['#text'], indent=4),"\n"
+#in this case there are 2 names for the organism 1- the name we use today 2- the scientific name 
+print json.dumps( docOrgName['uniprot']['entry']['organism']['name'][0]['#text'], indent=4)         #extracting name 1 for organism
+print json.dumps( docOrgName['uniprot']['entry']['organism']['name'][1]['#text'], indent=4),"\n"    #extracting name 2 for organism
 
 #query to print protein name
 query_uniPro="&limit=1&format=xml"
