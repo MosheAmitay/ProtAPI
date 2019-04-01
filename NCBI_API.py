0a1fa3a22b156383ee6abeb7a57bdde8fae8cee4
תמר סויונוב <soyonov@bio1.acad.jct.ac.il>
2019-04-01 16:03:10 +0300

    1.4.19

--- NCBI_API.py
+++ NCBI_API.py
@@ -35,24 +35,53 @@ handle.close()
 #fetch a record in text format
 #the id from NCBI you want to fetch enter string of id
 id_rec = record["IdList"][0]
+print "id rec: "
 handle2 = Entrez.efetch(db="nucleotide", id=id_rec, rettype="gb", retmode="text")
 #print handle2.read()
 
 handle2.close()
 
+dict_NCBI=dict()
+
+
 #fetch a record in xml format
 #the id from NCBI you want to fetch enter string of id
-handle3 = Entrez.efetch(db="nucleotide", id=id_rec, retmode="xml")
-print "handle3: " 
-print  handle3.read() 
+handle3 = Entrez.efetch(db="nucleotide", id=id_rec, rettype="gb", retmode="xml")
+print "handle3: "
 
-record3=  Entrez.read(handle3)
+record3=Entrez.read(handle3)
 record3=record3[0]
+print  json.dumps(record3,indent=4)
+
+
 #print details about the record
-print json.dumps(record3,indent = 4)
 print len(record3)
 print(record3.keys())
 print "strandedness: " +  record3["GBSeq_strandedness"]
 print "sequence: " + record3["GBSeq_sequence"]
 print "molecule type: " + record3["GBSeq_moltype"]
+
+#create heavy dictionary to record of the NCBI
+dict_NCBI["strandedness"]=record3["GBSeq_strandedness"]
+dict_NCBI["sequence"]=record3["GBSeq_sequence"]
+dict_NCBI["molecule type"]=record3["GBSeq_moltype"]
+dict_NCBI["organism"]=record3["GBSeq_source"]
+dict_NCBI["primary-accession"]=record3["GBSeq_primary-accession"]
+dict_NCBI["definition"]=record3["GBSeq_definition"]
+dict_NCBI["accession-version"]=record3["GBSeq_accession-version"]
+dict_NCBI["topology"]=record3["GBSeq_topology"]
+dict_NCBI["length"]=record3["GBSeq_length"]
+#dict_NCBI[""]=record3["GBSeq_feature-table"] #here. need to check on another record.. ask moshe whice parameters needs. mail..
+
+
+
+print "dict_NCBI: "
+print dict_NCBI
+
 handle3.close()
+
+#def basic_data_by_id(entry_id):
+
+#       handle3 = Entrez.efetch(db="nucleotide", id=entry_id, retmode="xml")
+#       print "gv"
+
