dd46aa7cc010e3046af8cf5c7ae76fc94b5f6801
Elisheva Powitz <powitz@bio1.acad.jct.ac.il>
2019-04-02 14:42:42 +0300

    2.4.19

--- elementTreeTry.py
+++ elementTreeTry.py
@@ -23,24 +23,24 @@ tree = ET.parse('Doc.xml')
 root = tree.getroot()
 #for child in root:
 #    print(child.tag, child.attrib)
-print root.iter()
-for elem in root.iter()
-        print elem.tag
+#print root.iter()
+my_list=([(elem.tag,elem.attrib) for elem in root.iter()])
+
+for i in my_list:
+	print (i)
 
 
-Full_Dict={
-"Gene Name:": entery['uniprot']['entry']['gene']['name']['#text'],
-"Organism Name:": entery['uniprot']['entry']['organism']['name'][0]['#text'],
-"Protein Full Name:": entery['uniprot']['entry']['protein']['recommendedName']['fullName'],
-"Comments:": entery['uniprot']['entry']['comment'][0]['text']['#text'],
-"Protein Sequence:": entery['uniprot']['entry']['sequence']['#text'],
-"Data Set:": json.dumps(entery['uniprot']['entry']['@dataset'], indent=4),
-"Date Of Creation:": json.dumps(entery['uniprot']['entry']['@created'], indent=4),
-"Date Of Last Modification:": json.dumps(entery['uniprot']['entry']['@modified'], indent=4)
+#Full_Dict={
+#"Gene Name:": entery['uniprot']['entry']['gene']['name']['#text'],
+#"Organism Name:": entery['uniprot']['entry']['organism']['name'][0]['#text'],
+#"Protein Full Name:": entery['uniprot']['entry']['protein']['recommendedName']['fullName'],
+#"Comments:": entery['uniprot']['entry']['comment'][0]['text']['#text'],
+#"Protein Sequence:": entery['uniprot']['entry']['sequence']['#text'],
+#"Data Set:": json.dumps(entery['uniprot']['entry']['@dataset'], indent=4),
+#"Date Of Creation:": json.dumps(entery['uniprot']['entry']['@created'], indent=4),
+#"Date Of Last Modification:": json.dumps(entery['uniprot']['entry']['@modified'], indent=4)
+#}
 
-
-
-}
 #for x, y in Full_Dict.items():
  #       print"\n"
   #      print(x, y)
