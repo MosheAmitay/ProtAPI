845fc9a774fafaae473da6262068cf5e4030b1d7
yiskaN <46136921+yiskaN@users.noreply.github.com>
2019-06-04 22:37:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from YNpdb import *
 
-YNpdb.search_pdb() #checks the first function
-YNpdb.print_all() #checks the second function
+dict_1 = YNpdb.search_pdb() #checks the first function
+dict_2 = YNpdb.print_all() #checks the second function
