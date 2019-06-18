1536ce42084fd4e9b08d5506069bb36891dc911c
yiskaN <46136921+yiskaN@users.noreply.github.com>
2019-06-18 14:46:47 +0300

    Update test.py

--- pdb/test.py
+++ pdb/test.py
@@ -2,5 +2,4 @@ from YNpdb import *
 
 ids = ['6QCW']
 colmns = ['chainLength','ligandId']
-dict_1 = YNpdb.search_pdb(ids, colmns) #checks the first function
-dict_2 = YNpdb.print_all(ids, colmns) #checks the second function
+dict = YNpdb.search_pdb(ids, colmns) 
