3f9e1b5a53154805fbc5d7a64f8ac9efcb0b6017
shir555 <46501397+shir555@users.noreply.github.com>
2019-06-23 07:49:59 +0300

    added return dictionary

--- NCBI_html/geoParse.py
+++ NCBI_html/geoParse.py
@@ -15,57 +15,83 @@ def strip_non_ascii(string):
 def parseGeo(accFile, field):
     data = pd.read_csv(accFile, sep="\n", header=None) #read the content with pandas
     data.columns = ["accession"]
+    values=dict()
     output=open('output.txt', 'w') #open a file for output
     for a in data["accession"]: #for each ID
+            numFound=0
             url = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc='+a
             source=requests.get(url).text #get the html page
             soup=bs(source,'lxml') #create beautiful soup object
             if soup.find_all("td"): #if the search exists
                     for element in soup.find_all("td"):
-                            if element.string==field: #find the wanted line
-				#put the information into the file
-                                    output.write("%s\n" % strip_non_ascii(element.next_sibling.next_sibling.text))
+                            if element.string==field & numFound==0: #find the wanted line
+                                    if field == "Samples":
+                                            print element.next_sibling.next_sibling
+                                            #all=""
+                                            #for sample in element.next_sibling.next_sibling:
+                                            #all=all+""
+                                    else:
+				            #put the information into the file or dictionary
+                                            numFound=numFound+1
+                                            values[a+"_"+field] = strip_non_ascii(element.next_sibling.next_sibling.text)
+                                            output.write("%s\n" % strip_non_ascii(element.next_sibling.next_sibling.text))
     output.close()
     print("the results are in output.txt")
+    return values
 
 def parseSRA(accFile, field):
     data = pd.read_csv(accFile, sep="\n", header=None) #read the content with pandas
     data.columns = ["accession"]
+    values=dict()
     output=open('output.txt', 'w') #open a file for output
     for a in data["accession"]: #for each ID
-	    if "SRX" in a: parseSRX(a)
+	    if "SRX" in a: parseSRX(a,output,values)
+    output.close()
+    print("the results are in output.txt")
+    return values
             
-def parseSRX(acc):
-	url = 'https://www.ncbi.nlm.nih.gov/sra/'+acc+'[accn]'
+def parseSRX(acc,output_file,values):
+        url = 'https://www.ncbi.nlm.nih.gov/sra/'+acc+'[accn]'
         source=requests.get(url).text #get the html page
         soup=bs(source,'lxml') #create beautiful soup object
-        if soup.find_all("div"):
-                for element in soup.find_all("div"):
-                        if field in element.text:if field=='Library':
-                                if field=='Library':
+        if soup.find("div", {"id": "ResultView"}): #if the data exists
+                for element in soup.find("div", {"id": "ResultView"}): #loop over the data and find the needed field
+                        if field in element.text:
+                                if field=='Library': #needs special data extraction
+                                        all=''
                                         for item in element.div:
-                                               output_file.write("%s\n" % strip_non_ascii(item.contents[0]+item.span.text))
-                                elif field=='Runs':
+                                                #write the title=contents[0] and the content=item.span.text
+                                                all=all+strip_non_ascii(item.contents[0]+item.span.text)+"\n"
+                                                output_file.write("%s\n" % strip_non_ascii(item.contents[0]+item.span.text))
+                                        values[acc+"_"+field]=all
+                                elif field=='Runs': #needs special data extraction
+                                        #write the header
+                                        all=strip_non_ascii(element.span.contents[0])+"\n"
                                         output_file.write("%s\n" % strip_non_ascii(element.span.contents[0])) 
                                         table=element.next_sibling
                                         for row in range(len(table.tbody.contents)):
                                                 for col in range(len(table.thead.tr.contents)):
                                                         if col == 0:
+                                                                #if it is the Run cell
+                                                                all=all+strip_non_ascii(table.thead.tr.contents[col].text+" #"+str(row+1)+": "+table.tbody.contents[row].contents[col].text)+"\n"
                                                                 output_file.write("%s\n" % strip_non_ascii(table.thead.tr.contents[col].text+" #"+str(row+1)+": "+table.tbody.contents[row].contents[col].text))
                                                         else:
+                                                                all=all+strip_non_ascii(table.thead.tr.contents[col].text+": "+table.tbody.contents[row].contents[col].text)+"\n"
                                                                 output_file.write("%s\n" % strip_non_ascii(table.thead.tr.contents[col].text+": "+table.tbody.contents[row].contents[col].text))
+                                        values[acc+"_"+field]=all
                                 else:
-					output_file.write("%s\n" % strip_non_ascii(element.span.contents[0]))
+                                        values[acc+"_"+field]=strip_non_ascii(element.span.contents[0])
+                                        output_file.write("%s\n" % strip_non_ascii(element.span.contents[0]))
 
 #this is a redirection function for future expansions
 def parse(db, accFile, field):
     if db=="geo":
-        parseGeo(accFile, field)
+        return parseGeo(accFile, field)
     if db=="sra":
-	parseSRA(accFile, field)
+	      return parseSRA(accFile, field)
 
 #here you get the information for the functions
-db=raw_input("please enter the database you need:   ")
-accFile=raw_input("please enter name of a file with the accessions:   ")
-field=raw_input("please enter the field your'e interested in extracting:   ")
-parse(db, accFile, field)
+db = raw_input("please enter the database you need:   ")
+accFile = raw_input("please enter name of a file with the accessions:   ")
+field = raw_input("please enter the field your'e interested in extracting:   ")
+print parse(db, accFile, field)
